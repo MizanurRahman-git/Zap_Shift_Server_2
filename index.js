5e1ff30ccf66a41a3b4d634d5daef806e74b79ef
Mizanur Rahman <mdmizanurrahmanhridoy5@gmail.com>
2026-08-09 12:54:38 +0600

    mongodb is not connecting

--- index.js
+++ index.js
@@ -1,18 +1,40 @@
-const express = require('express');
-const cors = require('cors')
-const app = express()
-require('dotenv').config()
-const port = process.env.PORT || 3000
+const express = require("express");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+const port = process.env.PORT || 3000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+// MiddleWere
+app.use(express.json());
+app.use(cors());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@crud-operation.zma1h9w.mongodb.net/?appName=CRUD-Operation`;
 
 
-// MiddleWere 
-app.use(express.json())
-app.use(cors())
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
 
-app.get('/', (req, res) => {
-  res.send('Zap-Shift-2 is Running....')
-})
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Zap-Shift-2 is Running....");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
