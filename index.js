945272ab9f02a24fed987dfc7d294755ca060f84
Mizanur Rahman <mdmizanurrahmanhridoy5@gmail.com>
2026-08-16 23:56:02 +0600

    added a post method for stripe

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 const dns = require("dns");
-dns.setServers([
-  "8.8.8.8",
-  "1.1.1.1"
-]);
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
 const express = require("express");
 const cors = require("cors");
-const stripe = require('stripe')(process.env.STRIPE_SECRET);
+const stripe = require("stripe")(process.env.STRIPE_SECRET);
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 3000;
@@ -15,70 +12,79 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@simple-crud-serv.sbd6kzc.mongodb.net/?appName=Simple-CRUD-Serv`
-
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@simple-crud-serv.sbd6kzc.mongodb.net/?appName=Simple-CRUD-Serv`;
 
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     await client.connect();
 
-    const db = client.db("zap_shift_db_2")
-    const parcelsCollection = db.collection('parcels')
+    const db = client.db("zap_shift_db_2");
+    const parcelsCollection = db.collection("parcels");
 
-    app.get('/parcels', async(req, res)=> {
-      const query = {}
-      const {email} = req.query
+    app.get("/parcels", async (req, res) => {
+      const query = {};
+      const { email } = req.query;
 
-      if(email){
-        query.senderEmail = email
+      if (email) {
+        query.senderEmail = email;
       }
 
-      const options = {sort:{createdAt: -1}}
+      const options = { sort: { createdAt: -1 } };
 
-      const cursor = parcelsCollection.find(query, options)
-      const result = await cursor.toArray()
-      res.send(result)
-    })
+      const cursor = parcelsCollection.find(query, options);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
-    app.post('/parcels', async(req, res)=>{
+    app.post("/parcels", async (req, res) => {
       const parcel = req.body;
       parcel.createdAt = new Date();
-      const result = await parcelsCollection.insertOne(parcel)
-      res.send(result)
-    })
+      const result = await parcelsCollection.insertOne(parcel);
+      res.send(result);
+    });
 
-    app.delete('/parcels/:id', async(req,res)=> {
-      const id = req.params.id
+    app.delete("/parcels/:id", async (req, res) => {
+      const id = req.params.id;
 
-      const query = {_id: new ObjectId(id)}
-      const result = await parcelsCollection.deleteOne(query)
-      res.send(result)
-    })
+      const query = { _id: new ObjectId(id) };
+      const result = await parcelsCollection.deleteOne(query);
+      res.send(result);
+    });
 
-    app.post('/create-checkout-session', async(req, res)=>{
+    app.post("/create-checkout-session", async (req, res) => {
       const paymentInfo = req.body;
       const session = await stripe.checkout.sessions.create({
-        
-      })
-    })
+        line_items: [
+          {
+            price: "{{PRICE_ID}}",
+            quantity: 1,
+          },
+        ],
+        mode: "payment",
+        success_url: `${process.env.SITE_DOMAIN}/dashboard/payment-success`,
+      });
+    });
 
-    app.get('/parcel/:parcelId', async(req,res)=>{
-      const id = req.params.parcelId
-      const query = {_id: new ObjectId(id)}
-      const result = await parcelsCollection.findOne(query)
-      res.send(result)
-    })
+    
+    app.get("/parcel/:parcelId", async (req, res) => {
+      const id = req.params.parcelId;
+      const query = { _id: new ObjectId(id) };
+      const result = await parcelsCollection.findOne(query);
+      res.send(result);
+    });
 
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
   } finally {
     // await client.close();
   }
